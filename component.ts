9cc31f9e4caeef5f392a6e56a59cbf6b68ed8244
huyle0001 <31173989+huyle0001@users.noreply.github.com>
2021-02-22 22:27:41 -0800

    Create component.ts

---
+++ component.ts
@@ -0,0 +1,53 @@
+//app.component.ts
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
+import { debounceTime, tap, switchMap, finalize } from 'rxjs/operators';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  searchMoviesCtrl = new FormControl();
+  filteredMovies: any;
+  isLoading = false;
+  errorMsg: string;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  ngOnInit() {
+    this.searchMoviesCtrl.valueChanges
+      .pipe(
+        debounceTime(500),
+        tap(() => {
+          this.errorMsg = "";
+          this.filteredMovies = [];
+          this.isLoading = true;
+        }),
+        switchMap(value => this.http.get("http://www.omdbapi.com/?apikey=[YOUR_KEY_HERE]=" + value)
+          .pipe(
+            finalize(() => {
+              this.isLoading = false
+            }),
+          )
+        )
+      )
+      .subscribe(data => {
+        if (data['Search'] == undefined) {
+          this.errorMsg = data['Error'];
+          this.filteredMovies = [];
+        } else {
+          this.errorMsg = "";
+          this.filteredMovies = data['Search'];
+        }
+
+        console.log(this.filteredMovies);
+      });
+  }
+}
