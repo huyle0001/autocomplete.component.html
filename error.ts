8be62bfe38d4b8fafa85ea6639cb534edd4db9e1
huyle0001 <31173989+huyle0001@users.noreply.github.com>
2021-03-03 20:28:44 -0800

    Update error.ts

--- error.ts
+++ error.ts
@@ -20,3 +20,29 @@ export class ErrorInterceptor implements HttpInterceptor {
   }
 
 }
+
+
+mport { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs/internal/operators';
+import { Router } from '@angular/router';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(private router: Router) { }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request)
+      .pipe(
+        catchError((err, caught: Observable<HttpEvent<any>>) => {
+          if (err instanceof HttpErrorResponse && err.status == 401) {
+            this.router.navigate(['login'], { queryParams: { returnUrl: request.url } });
+            return of(err as any);
+          }
+          throw err;
+        })
+      );
+  }
+}
